0d8c6117721d3561e90689c40bb9b176589d4bf2
kuniaki <kuniaki@iMac.local>
2021-12-26 12:00:22 +0900

    define target URL

--- uitest/test/uitest.py
+++ uitest/test/uitest.py
@@ -1,4 +1,5 @@
-Rfrom selenium import webdriver
+import os, sys, time, datetime, requests, json
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
@@ -12,11 +13,14 @@ o.add_argument('--disable-gpu')
 o.add_argument('--no-sandbox')
 o.add_argument('--window-size=1200x600')
 
+WEB_HOST = os.environ['WEB_HOST']
+WEB_PORT = int(os.environ['WEB_PORT'])
+WEBURL = f'http://{WEB_HOST}:{WEB_PORT}/'
+
 """
 Sample test
 """
 d = webdriver.Chrome(chromedriver_path, options=o)
-d.get('http://asahihdgrjenkinsslave1.eastus.cloudapp.azure.com')
+d.get(WEBURL)
 print(d.title)
 d.quit()
-
