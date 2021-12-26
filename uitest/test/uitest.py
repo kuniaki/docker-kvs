e86485eb8502b11680fe5bf94ca91b4b69f33231
kuniaki <kuniaki@iMac.local>
2021-12-26 11:28:06 +0900

    stertaed ui automation test

---
+++ uitest/test/uitest.py
@@ -0,0 +1,22 @@
+Rfrom selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+
+chrome_path = '/usr/bin/chromium-browser'
+chromedriver_path = '/usr/lib/chromium/chromedriver'
+o = Options()
+o.binary_location = '/usr/bin/chromium-browser'
+o.add_argument('--headless')
+o.add_argument('--disable-gpu')
+o.add_argument('--no-sandbox')
+o.add_argument('--window-size=1200x600')
+
+"""
+Sample test
+"""
+d = webdriver.Chrome(chromedriver_path, options=o)
+d.get('http://asahihdgrjenkinsslave1.eastus.cloudapp.azure.com')
+print(d.title)
+d.quit()
+
