fa1f583eb742501c7efa682afecf9022b6805544
kuniaki <kuniaki@iMac.local>
2021-12-27 13:16:28 +0900

    added necessary library

--- uitest/test/uitest.py
+++ uitest/test/uitest.py
@@ -1,5 +1,6 @@
 import os, sys, time, datetime,  json
 from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
