0ee0a8154ceec65a6ca83ebd80b29c842b12e05a
kuniaki <kuniaki@iMac.local>
2021-12-27 12:39:47 +0900

    test webdriver

--- uitest/test/uitest.py
+++ uitest/test/uitest.py
@@ -13,9 +13,29 @@ o.add_argument('--disable-gpu')
 o.add_argument('--no-sandbox')
 o.add_argument('--window-size=1200x600')
 
+SELENIUM_HUB_HOST = 127.0.0.1
+SELENIUM_HUB_PORT =  4444
+
+SELENIUM_HUB_HOST = os.environ['SELENIUM_HUB_HOST']
+SELENIUM_HUB_PORT = int(os.environ['SELENIUM_HUB_PORT'])
+HUBURL = f'http://{SELENIUM_HUB_HOST}:{SELENIUM_HUB_PORT}/wd/hub'
+
 WEB_HOST = os.environ['WEB_HOST']
 WEB_PORT = int(os.environ['WEB_PORT'])
 WEBURL = f'http://{WEB_HOST}:{WEB_PORT}/'
+
+print("FLOW1")
+
+driver = webdriver.Remote(
+            command_executor=HUBURL,
+            desired_capabilities=DesiredCapabilities.CHROME)
+
+print("FLOW2")
+
+driver.get(WEBURL)
+
+print("FLOW3")
+
 
 d = webdriver.Chrome(chromedriver_path, options=o)
 print(WEBURL)
