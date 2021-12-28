ada6dc0c1b66cb31d815403f102fef3727801883
kuniaki <kuniaki@iMac.local>
2021-12-28 10:56:18 +0900

    cleaned up uitest env

--- uitest/test/uitest.py
+++ uitest/test/uitest.py
@@ -1,4 +1,5 @@
 import os, sys, time, datetime, requests, json
+import logging
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
@@ -12,17 +13,24 @@ WEBURL = f'http://{WEB_HOST}:{WEB_PORT}/'
 WEBURL = 'http://asahihdgrjenkinsslave1.eastus.cloudapp.azure.com'
 APIURL = WEBURL + '/api/v1'
 
+logging.basicConfig(level=logging.DEBUG)
+mylogger= logging.getLogger()
+
 
 def test_get_success_nokey():
   clean_and_add_keys()
   try:
-    print("test_get_success_nokey")
+    print("deiver_elements webdriver")
     (driver, elems) = get_driver_elements()
-    print(elems)
+    print("deiver_elements webdriver-1")
     print(driver)
+    print("deiver_elements webdriver-2")
+    print(driver.title)
+    print("deiver_elements webdriver-3")
     elems['get-button'].click()
     time.sleep(1)
     test_name = sys._getframe().f_code.co_name
+    mylogger.info(test_name)
     take_screenshot(driver, test_name)
     assert elems['request-url'].text   == '/api/v1/keys/'
     assert elems['request-body'].text  == ''
@@ -35,39 +43,26 @@ def test_get_success_nokey():
     raise
 
 def get_driver_elements():
-  print("deiver_elements")
-  chrome_path = '/usr/bin/chromium-browser'
-  chromedriver_path = '/usr/lib/chromium/chromedriver'
-  options = Options()
-  o = Options()
-  o.binary_location = '/usr/bin/chromium-browser'
-  o.add_argument('--headless')
-  o.add_argument('--disable-gpu')
-  o.add_argument('--no-sandbox')
-  o.add_argument('--window-size=1200x600')
-  print("deiver_elements options")
+    chrome_path = '/usr/bin/chromium-browser'
+    chromedriver_path = '/usr/lib/chromium/chromedriver'
+    o = Options()
+    o.binary_location = '/usr/bin/chromium-browser'
+    o.add_argument('--headless')
+    o.add_argument('--disable-gpu')
+    o.add_argument('--no-sandbox')
+    o.add_argument('--window-size=1200x600')
 
-  s = Service(executable_path=chromedriver_path)
-  s.start()
-  d = webdriver.Remote(
-    s.service_url,
-    desired_capabilities=o.to_capabilities()
-  )
+    d = webdriver.Chrome(chromedriver_path, options=o)
 
-  print("deiver_elements webdriver")
-  print(d)
-  WEBURL = 'http://asahihdgrjenkinsslave1.eastus.cloudapp.azure.com'
-  d.get(WEBURL)
-  print(d.title)
-  print("deiver_elements URL")
-  elements = {}
-  for html_id in ['key', 'value', 
+    WEBURL = 'http://asahihdgrjenkinsslave1.eastus.cloudapp.azure.com'
+    d.get(WEBURL)
+    elements = {}
+    for html_id in ['key', 'value',
                  'get-button', 'post-button', 'put-button', 'delete-button',
                  'request-url', 'request-body', 'response-code', 'response-body']:
-    elements[html_id] = d.find_element_by_id(html_id)
-  print("deiver_elements ELEMENTS")
-  print(elements)
-  return (d, elements)
+     elements[html_id] = d.find_element_by_id(html_id)
+    print(d)
+    return (d, elements)
 
 def clean():
   r = requests.get(f'{APIURL}/keys/')
@@ -82,3 +77,8 @@ def clean_and_add_keys():
   assert r.status_code == 200
   r = requests.put(f'{APIURL}/keys/banana', data='yellow')
   assert r.status_code == 200
+
+def take_screenshot(driver, title):
+  today = datetime.datetime.today()
+  timestamp = today.strftime("%Y%m%d%H%M%S")
+  driver.save_screenshot(f'/images/{timestamp}-{title}.png')
